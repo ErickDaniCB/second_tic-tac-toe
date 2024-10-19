dc3c23ac0df5bd26ba23f00c25771947ac21fa8e
ErickDaniCB <erickdani10@hotmail.com>
2024-10-18 18:10:16 -0600

    Add overhaul to gameflow and win logic using cell objects

--- tic.js
+++ tic.js
@@ -1,110 +1,133 @@
-// Board factory 
-function board () {
+// Board factory
+function board() {
   let newBoard = () => {
-    let arr = []
-    for(let i = 0; i < 3; i++){
-      arr.push([])
+    let arr = [];
+    for (let i = 0; i < 3; i++) {
+      arr.push([]);
       for (let j = 0; j < 3; j++) {
-        arr[i].push(cell())
+        arr[i].push(cell());
       }
     }
-    return arr
-  }
+    return arr;
+  };
 
   return {
-    newBoard
-  }
+    newBoard,
+  };
 }
 
 // Cell factory
-function cell () {
-  let value = "X"
+function cell() {
+  let value = "none";
 
   const getCellValue = () => value;
-  
+
   const setCellValue = (playerSymbol) => {
     value = playerSymbol;
-  }
+  };
 
   return {
     getCellValue,
-    setCellValue
-  }
+    setCellValue,
+  };
 }
 
-function gameFlow (playerOneName = "Player One", playerTwoName = "Player Two") {
+// WINNER LOGIC
+
+function winnerLogic(board) {
+  let rows = "";
+  let columns = "";
+
+  const rowsUpdate = () => {
+    board.forEach((row) => {
+      row.forEach((obj) => {
+        rows += obj.getCellValue();
+      });
+    });
+  };
+
+  const columnsUpdate = () => {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        columns += board[j][i].getCellValue();
+      }
+    }
+  };
+
+  let checkWinner = (current) => {
+    let pattern = `${current}${current}${current}`;
+    rowsUpdate();
+    columnsUpdate();
+    if (rows.includes(`${pattern}`) || columns.includes(`${pattern}`)) {
+      console.log(`${current} wins!`);
+    }
+  };
+
+  return {
+    checkWinner,
+  };
+}
+
+function gameFlow(playerOneName = "Player One", playerTwoName = "Player Two") {
+  //create board
+  let boardOBj = board();
+  let thisBoard = boardOBj.newBoard();
+
+  //create winner logic
+  let winnerObj = winnerLogic(thisBoard);
+
+  //create players
   const playerOne = {
     name: playerOneName,
-    plays: "X"
-  }
+    plays: "X",
+  };
 
   const playerTwo = {
     name: playerTwoName,
-    plays: "O"
-  }
+    plays: "O",
+  };
 
+  // change current player
   let currentPlayer = playerOne;
 
   let switchPlayer = (current) => {
-    if(current === playerOne){
+    if (current === playerOne) {
       currentPlayer = playerTwo;
     } else {
       currentPlayer = playerOne;
     }
-  } 
-  
-  let rows = "";
-  let columns = "";
+  };
 
-
-  let checkWinner = (current) => {
-    for(let i = 0; i < 3; i++){
-      for (let j = 0; j < 3; j++) {
-        if(typeof thisBoard[i][j] === "string"){
-          rows += thisBoard[i][j]
-        } else {
-          rows += "n"
-        }
-      }
-    }
-    if(rows.includes(`${current.plays}${current.plays}${current.plays}`)){
-      console.log(`${current.name} wins`)
-    }
-
-    for(let i = 0; i < 3; i++){
-      for (let j = 0; j < 3; j++) {
-        if(typeof thisBoard[j][i] === "string"){
-          columns += thisBoard[j][i]
-        } else {
-          columns += "n"
-        }
-      }
-    }
-    if(columns.includes(`${current.plays}${current.plays}${current.plays}`)){
-      console.log(`${current.name} wins`)
-    }
-
-    
-
-  }
-
-  let boardOBj = board();
-  let thisBoard = boardOBj.newBoard()
-  console.table(thisBoard)
-  
   let placePlay = (row, column) => {
-    if(typeof thisBoard[row][column] === "object"){
-      thisBoard[row][column] = currentPlayer.plays;
-      console.clear();
-      console.table(thisBoard);
-      checkWinner(currentPlayer);
-      switchPlayer(currentPlayer)
+    const thisValue = thisBoard[row][column].getCellValue();
+    if ( thisValue === "none") {
+      thisBoard[row][column].setCellValue(currentPlayer.plays);
+      updateLogs();
+      winnerObj.checkWinner(currentPlayer);
+      switchPlayer(currentPlayer);
+    } else {
+      console.log("Cell already taken");
     }
-  }
+  };
+
+  //Logs
+  const updateLogs = () => {
+    let allLog = [];
+    thisBoard.forEach((row) => {
+      let rowLog = [];
+      row.forEach((obj) => {
+        rowLog.push(obj.getCellValue());
+      });
+      allLog.push(rowLog);
+    });
+    console.clear();
+    console.table(allLog);
+  };
+  updateLogs();
 
   return {
-    placePlay
-  }
+    placePlay,
+  };
 }
 
 let game1 = gameFlow();
